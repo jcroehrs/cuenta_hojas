3031f36a26af7860763d44b660cef1d9a8dd0001
Juan Carlos Roehrs <jcroehrs@gmail.com>
2021-03-04 12:04:43 -0300

    se agrego la opcion de carpeta

--- contar.py
+++ contar.py
@@ -1,6 +1,6 @@
+from PyPDF2 import PdfFileReader
 import argparse
 import os
-from PyPDF2 import PdfFileReader
 
 
 def extraer_information(pdf_ubicacion):
@@ -9,8 +9,7 @@ def extraer_information(pdf_ubicacion):
         informacion = pdf.getDocumentInfo()
         numero_de_paginas = pdf.getNumPages()
 
-    Resumen = f"""
-    Archivo {pdf_ubicacion}:     Paginas: {numero_de_paginas} """
+    Resumen = f""" Archivo {pdf_ubicacion}:     Paginas: {numero_de_paginas} """
 
     print(Resumen)
     return informacion
@@ -21,9 +20,29 @@ if __name__ == '__main__':
     parser.add_argument("-e", "--ejemplo",
                         help="Muestra un ejemplo", action="store_true")
     parser.add_argument("-f", "--fila", help="Nombre de archivo a procesar")
+    parser.add_argument(
+        "-d", "--directorio", help="Busca todos los archivos PDF del directorio actual")
     args = parser.parse_args()
+
     # Aquí procesamos lo que se tiene que hacer con cada argumento
+
     if args.ejemplo:
         print("contar -f <archivo>.pdf")
+        print("contar -d <ruta de la carpeta donde se encuentran los pdf>")
+        print('Si esta en la carpeta : contar -d ./')
+
     if args.fila:
         extraer_information(args.fila)
+
+    if args.directorio:
+      #  obtiene el contenido del directorio
+        directorio_def = args.directorio  # toma el directorio pasado por parametro
+        # lee el contenifo de la carpeta
+        contenido = os.listdir(directorio_def)
+        pdfs = []
+        for fichero in contenido:  # recorre el contenido del directorio para identificar los archivos PDF
+            if os.path.isfile(os.path.join(directorio_def, fichero)) and fichero.endswith('.pdf'):
+                pdfs.append(fichero)
+
+        for ARCHIVO in pdfs:  # Recorremos la lista de Archivos PDF y sacamos las hojas
+            extraer_information(directorio_def+ARCHIVO)
